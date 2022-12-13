59bcf3783472dbbef166ea71a9c7748b2eae7cfc
seanewest <3216121+seanewest@users.noreply.github.com>
2022-12-13 18:14:06 -0500

    Dockerfiles put into challenges dir

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-docker build . -t challenges
-docker build . -f Dockerfile.osint -t osint
-docker build . -f Dockerfile.osint2 -t osint2
-docker build . -f Dockerfile.pass -t pass
-docker build . -f Dockerfile.buf -t buf
-docker build . -f Dockerfile.story -t story
-docker build . -f Dockerfile.socialeng -t socialeng
+docker build challenges -f challenges/Dockerfile.collection -t challenges
+docker build challenges/osint -f challenges/osint/Dockerfile -t osint
+docker build challenges/osint2 -f challenges/osint2/Dockerfile -t osint2
+docker build challenges/pass -f challenges/pass/Dockerfile -t pass
+docker build challenges/buf -f challenges/buf/Dockerfile -t buf
+docker build challenges/story -f challenges/story/Dockerfile -t story
+docker build challenges/socialeng -f challenges/socialeng/Dockerfile -t socialeng
 
-nohup node server.js >/dev/null 2>&1  &
-nohup node_modules/.bin/budo index.js -P -p 80 >/dev/null 2>&1 &
+#nohup node server.js >/dev/null 2>&1  &
+#nohup node_modules/.bin/budo index.js -P -p 80 >/dev/null 2>&1 &
